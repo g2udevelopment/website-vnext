9891243308808ec01138ec6612091fe4307a4dc0
g2udevelopment <clshannon@protonmail.com>
2021-04-15 19:44:48 +0200

    calling github

--- cfpexchange-web-nuxtjs/components/Contributers.vue
+++ cfpexchange-web-nuxtjs/components/Contributers.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>List of contribs</div>
+  <div>
+    <ul class="flex">
+      <li class="m-4" v-for="contributor in contributors" :key="contributor.id">
+        <div>{{ contributor.login }}</div>
+        <div>Image</div>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      contributors: [],
+    }
+  },
+  async fetch() {
+    console.log("fetch called");
+    this.contributors = await fetch(
+      'https://api.github.com/repositories/128419453/contributors'
+    ).then((res) => res.json());
+  },
+}
+</script>
